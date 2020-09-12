fe253d1498638f608da82fdf9b712ca8c38273d6
esanvee <eesanvee18@students.desu.edu>
2020-09-12 01:37:42 -0400

    Added MonthlyLog class

---
+++ MonthlyTracker/test/MonthlyLog_test.py
@@ -0,0 +1,31 @@
+import unittest
+import datetime
+from MonthlyTracker.MonthlyLog import MonthlyLog
+from MonthlyTracker.Entry import Entry
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_add_entry(self):
+        test_month = MonthlyLog("September", "2020")
+        test_entry = Entry("test", 2.34, "none", datetime.date(2020, 9, 3))
+
+        test_month.add_entry(test_entry)
+
+        expected = 1
+        actual = test_month.get_entries_size()
+
+        self.assertEqual(expected, actual)
+
+    def test_print_entries(self):
+        test_month = MonthlyLog("December", "2022")
+        test_entry = Entry("test", 7.88, "none", datetime.date(2022, 12, 3))
+        test_entry2 = Entry("test", 3.12, "none", datetime.date(2022, 12, 13))
+
+        test_month.add_entry(test_entry)
+        test_month.add_entry(test_entry2)
+
+        expected = 'test 7.88 none 2022-12-03\ntest 3.12 none 2022-12-13\n'
+        actual = test_month.print_entries()
+
+        self.assertEqual(expected, actual)
